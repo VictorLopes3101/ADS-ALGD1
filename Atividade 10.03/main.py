eb2de2fb53a4c003ed29d17179056cec3ff3cb8f
Victor Lopes Fernandes <41849718+VictorLopes3101@users.noreply.github.com>
2020-03-10 22:06:01 -0300

    Add files via upload

---
+++ Atividade 10.03/main.py
@@ -0,0 +1,26 @@
+from random import randint
+
+class pastry_pos():
+
+    def main(self):
+        initial_amount = 0
+        self.sum(initial_amount)
+        pass
+    
+    def sum(self, amount):
+        print('Instuction:\n1 - This program dont check if you type e int value, then PLEASE type ONLY numbers\n2 - For exit the loop type 0')
+        sales = 0
+        actual_amount = amount 
+        run_condition = True
+        while run_condition:
+            price = int(input('Value: '))
+            if price == 0:
+                break
+            sales += 1
+            actual_amount += price
+        print(f'Finish, you did {sales} sales and you actual amount is {actual_amount}')
+        pass
+
+if __name__ == '__main__':
+    run = pastry_pos()
+    run.main()
